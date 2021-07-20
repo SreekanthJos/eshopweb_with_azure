968a90e8a51aeed7a94ab52684e9b932046435c3
Sreekanth Josyula <Sreekanth.Josyula@thomsonreuters.com>
2021-07-20 09:16:13 +0300

    service bus changes

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -57,8 +57,8 @@ namespace Microsoft.eShopWeb.ApplicationCore.Services
             var order = new Order(basket.BuyerId, shippingAddress, items);
           
             await _orderRepository.AddAsync(order);
-            // await UploadOrders(items);
-            await CreateDeliveryForOrder(order);
+             await UploadOrders(items);
+            //await CreateDeliveryForOrder(order);
         }
                
         private async Task CreateDeliveryForOrder(Order order)
@@ -85,8 +85,9 @@ namespace Microsoft.eShopWeb.ApplicationCore.Services
                 orderedItems.Add(new OrderedItem { ItemId = item.ItemOrdered.CatalogItemId, Quantity = item.Units });
             }
             var json = JsonConvert.SerializeObject(orderedItems);
-            var functionUrl = "https://sjorderreserver.azurewebsites.net/api/OrderItemsReserver?code=9M1/uP4tY52UOBkmOJm3BYtHT6pEvl4/HkISdpkgpy78O7gUwe1gxQ==";
-           //var requestData = new StringContent(json, Encoding.UTF8, "application/json");
+            var functionUrl = "https://sjorderreserver.azurewebsites.net/api/OrderItemsReserver?code=iXzDgYSevq0A3tuaRbJgcagLZHlFJs/HIEoxNgumPKMTnEbCSKVPSg==";
+                //"https://sjorderreserver.azurewebsites.net/api/OrderItemsReserver?code=6cP9es/k5XF9TJvr2PRgZmuvPRZUx9DZUauS8pa8WNUUuNQJJi6UoQ==";
+                //var requestData = new StringContent(json, Encoding.UTF8, "application/json");
             using (HttpClient client = new HttpClient())
             using (var request = new HttpRequestMessage(HttpMethod.Post, functionUrl))
             using (HttpContent content = CreateHttpContent(orderedItems))
--- src/OrderItemsReserver/OrderItemsReserver.cs
+++ src/OrderItemsReserver/OrderItemsReserver.cs
@@ -11,6 +11,7 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System.Configuration;
 using Microsoft.Extensions.Configuration;
+using Azure.Messaging.ServiceBus;
 
 namespace OrderItemsReserver
 {
@@ -21,38 +22,58 @@ namespace OrderItemsReserver
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log, ExecutionContext executionContext)
         {
-            var body = await new StreamReader(req.Body).ReadToEndAsync();
-            var fileName = "order_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".json";
-            var filePath = executionContext.FunctionAppDirectory +"/" + fileName;
+            try
+            {
+                var body = await new StreamReader(req.Body).ReadToEndAsync();
+                var fileName = "order_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".json";
+                var filePath = executionContext.FunctionAppDirectory + "/" + fileName;
 
-            File.WriteAllText(filePath, body);
-            //using (var fs = File.Create(fileName))
-            //{
-            //    //JsonSerializer serializer = new JsonSerializer();
-            //    //serializer.Formatting = Formatting.Indented;
-            //    //serializer.Serialize(file, body);
-            //}
-            await CreateBlob(filePath, executionContext);
-            log.LogInformation($"{fileName} uploaded successfully..");
+                File.WriteAllText(filePath, body);
+                //using (var fs = File.Create(fileName))
+                //{
+                //    //JsonSerializer serializer = new JsonSerializer();
+                //    //serializer.Formatting = Formatting.Indented;
+                //    //serializer.Serialize(file, body);
+                //}
+                await CreateBlob(filePath, executionContext);
+                log.LogInformation($"{fileName} uploaded successfully..");
 
-            //string name = req.Query["name"];
+                //string name = req.Query["name"];
 
 
 
-            string responseMessage =
-                 $"This HTTP triggered function executed successfully. {fileName} uploaded successfully..";
-               
-            return new OkObjectResult(responseMessage);
+                string responseMessage =
+                     $"This HTTP triggered function executed successfully. {fileName} uploaded successfully..";
+
+                return new OkObjectResult(responseMessage);
+            }
+            catch (Exception ex)
+            {
+                var config = new ConfigurationBuilder()
+                          .SetBasePath(executionContext.FunctionAppDirectory)
+                          .AddJsonFile("local.settings.json", true, true)
+                          .AddEnvironmentVariables().Build();
+
+                var client = new ServiceBusClient(config["SBConnestionString"]);
+                var sender = client.CreateSender(config["QueueName"]);
+                var message = "Order creation failed, please try again!!";
+                await sender.SendMessageAsync(new ServiceBusMessage(message));
+
+                throw (ex);
+            }
+
         }
         private static async Task<string> CreateBlob(string filePath,ExecutionContext executionContext)
         {
-            try
-            {
-                var config = new ConfigurationBuilder()
+            var config = new ConfigurationBuilder()
                            .SetBasePath(executionContext.FunctionAppDirectory)
                            .AddJsonFile("local.settings.json", true, true)
                            .AddEnvironmentVariables().Build();
 
+            
+            try
+            {
+                
                 CloudStorageAccount storageAccount = CloudStorageAccount.Parse(config["AzureWebJobsStorage"]);
                 CloudBlobClient client = storageAccount.CreateCloudBlobClient();
                 CloudBlobContainer cloudBlobContainer = client.GetContainerReference("orderscontainer");
@@ -60,7 +81,7 @@ namespace OrderItemsReserver
                 CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference("orders" + DateTime.Now.ToString("yyyyMMddHHmmssffff"));
 
                 await cloudBlockBlob.UploadFromFileAsync(filePath);
-
+               
                 return cloudBlockBlob.Uri.AbsoluteUri;
             }
             catch(Exception ex)
@@ -68,8 +89,6 @@ namespace OrderItemsReserver
                 throw (ex);
             }
         }
-
-        
 
     }
 }
